7fc1edb588032eee11b6719411c6d7c3bc89d0c2
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2024-05-22 22:36:59 -0300

    estudo abertura compilado no terminal

---
+++ src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Seja bem vindo!");
+    }
+}
+//Abrir o compilado com sucesso no terminal!
